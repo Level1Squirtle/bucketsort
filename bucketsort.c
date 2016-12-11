a33259d978b17a7f47db69bbb0efd02e303d70a2
Level1Squirtle <michaelfranklin@sandiego.edu>
2016-12-10 21:39:59 -0800

    Something is not working

--- bucketsort.c
+++ bucketsort.c
@@ -22,9 +22,10 @@ int main(void) {
 
 //  intialize variables
 	int my_rank, comm_size, arr_size, s;
-	long * arrSerial, * arrParallel, * temp, * samples;
+	long * arrSerial, * arrParallel, * temp, * samples, * firstSubArray, * finalSubArray;
 	struct timeval tv1, tv2;
 	double serialTime, parallelTime;
+	MPI_Comm comm;
 
 
 
@@ -32,6 +33,7 @@ int main(void) {
 	MPI_Init(NULL, NULL);
 	MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	comm = MPI_COMM_WORLD;
 	
 	if(my_rank == 0) {
 		printf("Please enter an array size:");
@@ -43,7 +45,7 @@ int main(void) {
 		temp = malloc(sizeof(long)*arr_size);
 		// allocates space for arrays
 		int i;
-		srand(0);
+		srand(2);
 		for(i = 0; i < arr_size; i++) {
 			arrSerial[i] = rand();
 			arrParallel[i] = arrSerial[i];
@@ -63,43 +65,48 @@ int main(void) {
 		gettimeofday(&tv1, NULL); //start parallel timing
 
 
-
-		//TODO block distribute arrParallel
-
-
-
 		s = 10 * comm_size * (log(arr_size)/log(2));//computes the size of the sample array
 		
 		samples = malloc(sizeof(long)*s);//allocates samples, now that we have the size
-
+		
 		for(i = 0; i < s; i++) {
-			samples[i] = arrParallel[srand(time(NULL)) % arr_size];
+		        samples[i] = arrParallel[rand() % arr_size];
 		}//assigns samples random values from arrParallel
-
-
+		mergeSortSerial(0, s - 1, samples, temp);
+		printf("Samples Sorted:/n");
+		for(i = 0; i < s; i++) {
+		  printf("%lu", samples[i]);
+		}
 
 		
 	}//end if statement for process 0
+
+	//MPI_Bcast(arr_size, 1, MPI_INT, 0, comm); 
+
+	//firstSubArray = malloc(sizeof(long)*arr_size/comm_size);
+	
+	//MPI_Scatter(arrParallel, arr_size, MPI_LONG, subArray, arr_size, MPI_LONG, 0, comm); 
+	// block distributes arrParallel
 
 	//TODO implement sorting algorithm
 
-	if(my_rank == 0) {
+	//if(my_rank == 0) {
 
-		gettimeofday(&tv2, NULL); //stop parallel timing
-    		parallelTime = (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec);
-
+	//	gettimeofday(&tv2, NULL); //stop parallel timing
+    	//	parallelTime = (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec);
+	//
 		
-		validateParallelSort(arr_size, arrSerial, arrParallel);		
+	//	validateParallelSort(arr_size, arrSerial, arrParallel);		
 		//process 0 validates arrParallel with arrSerial
 
 		// Print results.
-    		printf("Number of Processes = %d\nArray Size = %d\nSerial time = %e\n", comm_size, arr_size, serialTime);
-    		printf("Parallel time = %e\n", parallelTime);
-    		double speedup = serialTime / parallelTime;
-    		double efficiency = speedup / comm_size;
-    		printf("Speedup = %e\n", speedup);
-    		printf("Efficiency = %e\n", efficiency);
-	}//end if statement for process 0
+    	//	printf("Number of Processes = %d\nArray Size = %d\nSerial time = %e\n", comm_size, arr_size, serialTime);
+    	//	printf("Parallel time = %e\n", parallelTime);
+    	//	double speedup = serialTime / parallelTime;
+    	//	double efficiency = speedup / comm_size;
+    	//	printf("Speedup = %e\n", speedup);
+    	//	printf("Efficiency = %e\n", efficiency);
+	//}//end if statement for process 0
 
 	MPI_Finalize();
 
@@ -107,15 +114,6 @@ int main(void) {
 }
 
 
-void distributeArray(??) {
-//TODO Implement somehow
-}
-
-void choosePivots(??) {
-//TODO implement somehow
-}
-
-void
 
 
 
@@ -172,7 +170,6 @@ void validateSerialSort(int array_size, long * array) {
 			return;
 		}
 	}
-	printf("Array is sorted");
 	return;
 }
 
